267bbcc098a5baee9169ce177ffd9aa0adde994d
Samuel <samutfp@gmail.com>
2022-09-30 14:29:11 -0300

    Troca de fig

--- main.py
+++ main.py
@@ -1,95 +1,28 @@
-class TADFracao:
-    num = 1
-    denominador = 1
+def maxtroca_de_figurinhas(fig_da_maria, fig_do_joao):
+    i = 0
+    j = 0
+    control = 0
+    quant = 0
 
-    def _init_(self, num, denominador):
-        self.num = num
-        self.denominador = denominador
+    if len(fig_da_maria) < len(fig_do_joao):  
+        for i in range(len(fig_da_maria)):
+            for j in range(len(fig_do_joao)):
+                if fig_da_maria[i] == fig_do_joao[j]:
+                    control += 1
+        quant = len(fig_da_maria) - control
 
-    def adicionar(self, fracao):
-        num = (self.num * fracao.denominador) + (fracao.num *
-                                                       self.denominador)
-        deno = (self.denominador * fracao.denominador)
-        return Fracao(num, deno)
+    elif len(fig_do_joao) < len(fig_da_maria):
+        for i in range(len(fig_do_joao)):
+            for j in range(len(fig_da_maria)):
+                if fig_do_joao[i] == fig_da_maria[j]:
+                    control += 1
+        quant = len(fig_do_joao) - control
+                                                          
+    else:
+        for i in range(len(fig_da_maria)):
+            for j in range(len(fig_do_joao)):
+                if fig_da_maria[i] == fig_do_joao[j]:
+                    control += 1
+        quant = len(fig_da_maria) - control
 
-    def sub(self, fracao):
-        if (self.denominador
-                == fracao.denominador) & (self.num != fracao.num):
-            num = (self.num - fracao.num)
-            denominador = (self.denominador)
-        elif (self.denominador == fracao.denominador) & (self.num
-                                                         == fracao.num):
-            num = 0
-            deno = 0
-        else:
-            num = (self.num * fracao.denominador) - (fracao.num *
-                                                           self.denominador)
-            deno = (self.denominador * fracao.denominador)
-        return Fracao(num, deno)
-
-    def multiplicar(self, fracao):
-        num = (self.num * fracao.num)
-        deno = (self.denominador * fracao.denominador)
-        return Fracao(num, deno)
-
-    def infinito():
-        i = 0
-        while True:
-            yield i
-            i += 1
-
-    def simplificar(self):        
-      loop = True
-      j = 2
-      
-      while loop == True:               
-        control = False
-        if self.num / j == 1 or self.denominador / j == 1:
-          loop = False
-        
-        if self.num % j == 0 and self.denominador % j == 0:       
-          self.num = self.num / j
-          self.denominador = self.denominador / j
-          control = True
-          
-        else:          
-          j += 1
-          control = True
-
-        if control == False:
-          loop = False
-          
-          
-      return fracao(self.num, self.denominador)
-
-    def gcd(self):
-        while self.denominador != 0:
-            t = self.denominador
-            self.denominador = self.num % self.denominador
-            self.num = t
-        return self.num
-
-    def reduce(self):
-        greatest = self.gcd(self)
-        num = self.num / greatest
-        deno = self.denominador / greatest
-        return Fracao(num, deno)
-
-    def resolver(self):
-        return self.num / self.denominador
-
-    def _str_(self):
-        return f"{self.num}/{self.denominador}"
-
-
-fracao1 = Fracao(24,60)
-print(f"Simplificacao: {fracao1.simplificar()}")
-
-fracao1 = Fracao(56,75)
-print(f"Simplificacao: {fracao1.simplificar()}")
-
-fracao1 = Fracao(8,20)
-print(f"Simplificacao: {fracao1.simplificar()}")
-
-fracao1 = Fracao(200,350)
-print(f"Simplificacao: {fracao1.simplificar()}")
+    return quant1
